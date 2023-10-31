fa8e508abe5ea5dddc9d3b5f8b913ed7a80447c4
Muhammad Hutomo Padmanaba <mhutomop@gmail.com>
2023-10-31 14:10:34 +0700

    feat(push-pull): using push-pull

--- app.cpp
+++ app.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+
 #include <zmq.hpp>
 #include <zmq_addon.hpp>
 
@@ -11,12 +12,9 @@ int main()
     // initialize the zmq context with a single IO thread
     zmq::context_t context{1};
 
-    // construct a SUB (subscriber) socket and connect to interface
-    zmq::socket_t subscriber{context, zmq::socket_type::sub};
-    subscriber.connect("tcp://localhost:5555");
-
-    // set topics you want to sub
-    subscriber.set(zmq::sockopt::subscribe, "Hello");
+    // construct a PULL socket and connect to interface
+    zmq::socket_t pullSocket{context, zmq::socket_type::pull};
+    pullSocket.connect("tcp://localhost:5555");
 
     std::cout << "Subscriber listen!" << std::endl;
 
@@ -24,12 +22,11 @@ int main()
         // Receive all parts of the message
         std::vector<zmq::message_t> recv_msgs;
         zmq::recv_result_t result =
-            zmq::recv_multipart(subscriber, std::back_inserter(recv_msgs));
+            zmq::recv_multipart(pullSocket, std::back_inserter(recv_msgs));
         assert(result && "recv failed");
-        assert(*result == 2);
+        assert(*result == 1);
 
-        std::cout << "Subscriber: [" << recv_msgs[0].to_string() << "] "
-            << recv_msgs[1].to_string() << std::endl;
+        std::cout << "Received: [" << recv_msgs[0].to_string() << "] " << std::endl;
         std::this_thread::sleep_for(std::chrono::microseconds(100));
     }
 
